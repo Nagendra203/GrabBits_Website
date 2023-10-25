dc94528f1608e5b859db809ef4b1c7aa2130ca70
Nagendra203 <144248059+Nagendra203@users.noreply.github.com>
2023-10-25 10:54:18 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,19 +1,49 @@
-import mongoose from "mongoose";
-import { Server } from "../utils/config.js";
+// Require the MongoDB Node.js driver
+const { MongoClient } = require("mongodb");
 
-const connectDB = async () => {
+// Connection URL and database name
+const url = "mongodb://183.82.127.101:27017";
+const dbName = "myDatabase";
+
+// Function to connect to the MongoDB server
+async function connectToDatabase() {
   try {
-    console.log(Server.MONGO_URI);
-    mongoose.set("strictQuery", true);
-    const conn = await mongoose.connect(Server.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db(dbName);
+    return db;
   } catch (error) {
-    console.log(`Error: ${error.message}`);
-    process.exit(1);
+    console.error("Error connecting to the database:", error);
+    return null;
   }
-};
+}
 
-export default connectDB;
+// Function to insert a document into a collection
+async function insertDocument(collectionName, document) {
+  const db = await connectToDatabase();
+  if (!db) return;
+
+  const collection = db.collection(collectionName);
+
+  try {
+    const result = await collection.insertOne(document);
+    console.log("Document inserted:", result.insertedId);
+  } catch (error) {
+    console.error("Error inserting document:", error);
+  }
+}
+
+// Function to find documents in a collection based on a query
+async function findDocuments(collectionName, query) {
+  const db = await connectToDatabase();
+  if (!db) return;
+
+  const collection = db.collection(collectionName);
+
+  try {
+    const cursor = await collection.find(query);
+    const documents = await cursor.toArray();
+    console.log("Found documents:", documents);
+  } catch (error) {
+    console.error("Error finding documents:", error);
+  }
+}
